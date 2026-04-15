9619cd9ce908d9ceaeac091f4e9b9a697c0875f0
Restr32 <s33489@pjwstk.edu.pl>
2026-04-16 01:25:33 +0200

    nie pozwolenie usunąc sali dla której istnieją rezerwacje

--- zad5/Controllers/roomsController.cs
+++ zad5/Controllers/roomsController.cs
@@ -4,17 +4,31 @@ namespace zad5.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-
 public class roomsController : ControllerBase {
     public static List<redRum> rooms = new List<redRum>
     {
-        new redRum{id = 1, name = "B01", buildingCode = "B", floor = -1, capacity = 16, hasProjector = true, isActive = true},
-        new redRum{id = 2, name = "B123", buildingCode = "B", floor = 1, capacity = 24, hasProjector = true, isActive = true},
-        new redRum{id = 3, name = "C1", buildingCode = "C", floor = 1, capacity = 200, hasProjector = false, isActive = true},
-        new redRum{id = 4, name = "A360", buildingCode = "A", floor = 3, capacity = 22, hasProjector = true, isActive = false},
-        new redRum{id = 5, name = "H411", buildingCode = "H", floor = 4, capacity = 32, hasProjector = true, isActive = true}
-        
+        new redRum
+        {
+            id = 1, name = "B01", buildingCode = "B", floor = -1, capacity = 16, hasProjector = true, isActive = true
+        },
+        new redRum
+        {
+            id = 2, name = "B123", buildingCode = "B", floor = 1, capacity = 24, hasProjector = true, isActive = true
+        },
+        new redRum
+        {
+            id = 3, name = "C1", buildingCode = "C", floor = 1, capacity = 200, hasProjector = false, isActive = true
+        },
+        new redRum
+        {
+            id = 4, name = "A360", buildingCode = "A", floor = 3, capacity = 22, hasProjector = true, isActive = false
+        },
+        new redRum
+        {
+            id = 5, name = "H411", buildingCode = "H", floor = 4, capacity = 32, hasProjector = true, isActive = true
+        }
     };
+
     [HttpGet]
     public IActionResult GetAll() {
         return Ok(rooms);
@@ -40,9 +54,9 @@ public class roomsController : ControllerBase {
 
     [HttpPost]
     public IActionResult Create(redRum tmpRoom) {
-        int tmpId = rooms.Any() ? rooms.Max(r => r.id)+1 : 1;
+        int tmpId = rooms.Any() ? rooms.Max(r => r.id) + 1 : 1;
         tmpRoom.id = tmpId;
-        
+
         rooms.Add(tmpRoom);
         return CreatedAtAction(nameof(GetById), new { id = tmpRoom.id }, tmpRoom);
     }
@@ -50,7 +64,7 @@ public class roomsController : ControllerBase {
     [HttpPut("{id}")]
     public IActionResult Update(int id, [FromBody] redRum tmpRoom) {
         var livingRum = rooms.FirstOrDefault(r => r.id == id);
-        if (livingRum==null)
+        if (livingRum == null)
         {
             return NotFound();
         }
@@ -68,9 +82,15 @@ public class roomsController : ControllerBase {
     [HttpDelete("{id}")]
     public IActionResult Delete(int id) {
         var tmpRoom = rooms.FirstOrDefault(r => r.id == id);
-        if (tmpRoom==null)
+        if (tmpRoom == null)
         {
             return NotFound();
+        }
+
+        var hasReservations = reservationsController.reserv.Any(r => r.roomId == id);
+        if (hasReservations)
+        {
+            return Conflict("Nie można usunąć sali, bo istnieją rezerwacje");
         }
 
         rooms.Remove(tmpRoom);
