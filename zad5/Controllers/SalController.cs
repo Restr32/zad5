782081ac406d7b9c06001381b254f336f18ae2b7
Restr32 <s33489@pjwstk.edu.pl>
2026-04-16 00:05:19 +0200

    Dodanie do kontrolera akcji get dla wszystkich i po id

--- zad5/Controllers/SalController.cs
+++ zad5/Controllers/SalController.cs
@@ -6,5 +6,30 @@ namespace zad5.Controllers;
 [Route("[controller]")]
 
 public class SalController : ControllerBase {
+    public static List<redRum> rooms = new List<redRum>
+    {
+        new redRum{id = 1, name = "B01", buildingCode = "B", floor = -1, capacity = 16, hasProjector = true, isActive = true},
+        new redRum{id = 2, name = "B123", buildingCode = "B", floor = 1, capacity = 24, hasProjector = true, isActive = true},
+        new redRum{id = 3, name = "C1", buildingCode = "C", floor = 1, capacity = 200, hasProjector = false, isActive = true},
+        new redRum{id = 4, name = "A360", buildingCode = "A", floor = 3, capacity = 22, hasProjector = true, isActive = false},
+        new redRum{id = 5, name = "H411", buildingCode = "H", floor = 4, capacity = 32, hasProjector = true, isActive = true}
+        
+    };
+    [HttpGet]
+    public IActionResult GetAll() {
+        return Ok(rooms);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetById(int id) {
+        var room = rooms.FirstOrDefault(r => r.id == id);
+        if (room == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(room);
+    }
+    
     
 }
---
+++ zad5/redRum.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.VisualBasic.CompilerServices;
+
+namespace zad5;
+
+public class redRum {
+    public int id { get; set; }
+    [Required(ErrorMessage = "Wymagana nazwa")]
+    public string name { get; set; }
+    [Required(ErrorMessage = "Wymagany jest kod budynku")]
+    public string buildingCode { get; set; }
+    public int floor { get; set; }
+    [Range(1, int.MaxValue, ErrorMessage = "Pojemość musi być większa od zera")]
+    public int capacity { get; set; }
+    public bool hasProjector { get; set; }
+    public bool isActive { get; set; }
+    
+    public redRum(){}
+}
