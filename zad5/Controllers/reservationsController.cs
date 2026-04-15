81abaf3bdaeadcbfb421362006f12fa4799ccef9
Restr32 <s33489@pjwstk.edu.pl>
2026-04-16 01:36:30 +0200

    dodane przykładowe rezerwacje

--- zad5/Controllers/reservationsController.cs
+++ zad5/Controllers/reservationsController.cs
@@ -6,7 +6,45 @@ namespace zad5.Controllers;
 [Route("[controller]")]
 
 public class reservationsController : ControllerBase {
-    public static List<Reservation> reserv = new List<Reservation>();
+    public static List<Reservation> reserv = new List<Reservation>
+    {
+        new Reservation
+        {
+            id = 1, roomId = 1, organizerName = "dr inż. Kowalski", topic = "PPJ",
+            date = new DateOnly(2026, 05, 10), startTime = new TimeOnly(08, 00),
+            endTime = new TimeOnly(10, 00), status = "confirmed"
+        },
+        new Reservation
+        {
+            id = 2, roomId = 2, organizerName = "mgr Nowak", topic = "SWB",
+            date = new DateOnly(2026, 05, 10), startTime = new TimeOnly(11, 00),
+            endTime = new TimeOnly(13, 00), status = "planned"
+        },
+        new Reservation
+        {
+            id = 3, roomId = 3, organizerName = "Prof. Wiśniewski", topic = "ABPD",
+            date = new DateOnly(2026, 05, 11), startTime = new TimeOnly(09, 00),
+            endTime = new TimeOnly(11, 00), status = "confirmed"
+        },
+        new Reservation
+        {
+            id = 4, roomId = 5, organizerName = "dr Malinowski", topic = "TPO",
+            date = new DateOnly(2026, 05, 12), startTime = new TimeOnly(10, 00),
+            endTime = new TimeOnly(12, 00), status = "confirmed"
+        },
+        new Reservation
+        {
+            id = 5, roomId = 1, organizerName = "mgr Adamiak", topic = "JJP",
+            date = new DateOnly(2026, 05, 12), startTime = new TimeOnly(14, 00),
+            endTime = new TimeOnly(16, 00), status = "planned"
+        },
+        new Reservation
+        {
+            id = 6, roomId = 4, organizerName = "dr Zieliński", topic = "RBD",
+            date = new DateOnly(2026, 05, 13), startTime = new TimeOnly(08, 00),
+            endTime = new TimeOnly(10, 00), status = "confirmed"
+        }
+    };
 
     [HttpGet]
     public IActionResult GetAll() {
